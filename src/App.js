1d86fec4ff814f6d4cc7c11afcd5863fe05d32fb
Meet Patel <meetpatel4567.mp@gmail.com>
2023-11-09 07:04:30 -0500

    News App almost Ready

--- src/App.js
+++ src/App.js
@@ -1,42 +1,124 @@
-import React, { Component } from 'react'
-import Navbar from './components/Navbar'
-import News from './components/News'
-import { BrowserRouter,Routes,Route, } from 'react-router-dom';
-import LoadingBar from 'react-top-loading-bar'
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export class App extends Component {
-
-  apikey=process.env.API_KEY
-  state={
-    progress:0
-  }
-  setProgress=(progress)=>{
-    this.setState({progress:progress})
-  }
+  apikey = process.env.API_KEY;
+  state = {
+    progress: 0,
+  };
+  setProgress = (progress) => {
+    this.setState({ progress: progress });
+  };
   render() {
     return (
       <div>
-        <BrowserRouter>   
-           <Navbar></Navbar>
-           <LoadingBar
-        color='#f11946'
-        progress={this.state.progress}
-        onLoaderFinished={() => this.setProgress(0)}
-      />
-           <Routes>
-            <Route exact path='/' element={<News setProgress={this.setProgress}apikey={this.apikey} key="general" category={"General"} pagesize={18} />} />
-            <Route exact path='/business' element={<News setProgress={this.setProgress}apikey={this.apikey} key="business" category={"Business"} pagesize={18} />} />
-            <Route exact path='/entertainment' element={<News setProgress={this.setProgress}apikey={this.apikey} key="entertainment" category={"Entertainment"} pagesize={18} />} />
-            <Route exact path='/health' element={<News setProgress={this.setProgress}apikey={this.apikey} key="health" category={"Health"} pagesize={18} />} />
-            <Route exact path='/science' element={<News setProgress={this.setProgress}apikey={this.apikey} key="science" category={"Science"} pagesize={18} />} />
-            <Route exact path='/sports' element={<News setProgress={this.setProgress}apikey={this.apikey} key="sports" category={"Sports"} pagesize={18} />} />
-            <Route exact path='/technology' element={<News setProgress={this.setProgress}apikey={this.apikey} key="technology" category={"Technology"} pagesize={18} />} />
+        <BrowserRouter>
+          <Navbar></Navbar>
+          <LoadingBar
+            color="#f11946"
+            progress={this.state.progress}
+            onLoaderFinished={() => this.setProgress(0)}
+          />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="general"
+                  category={"General"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/business"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="business"
+                  category={"Business"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/entertainment"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="entertainment"
+                  category={"Entertainment"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/health"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="health"
+                  category={"Health"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/science"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="science"
+                  category={"Science"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/sports"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="sports"
+                  category={"Sports"}
+                  pagesize={18}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/technology"
+              element={
+                <News
+                  setProgress={this.setProgress}
+                  apikey={this.apikey}
+                  key="technology"
+                  category={"Technology"}
+                  pagesize={18}
+                />
+              }
+            />
           </Routes>
         </BrowserRouter>
-
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import load from './load.gif'
+import React, { Component } from "react";
+import load from "./load.gif";
 export class Spinner extends Component {
   render() {
     return (
-      <div className='text-center'>
-        <img src={load} alt='loading'/>
+      <div className="text-center">
+        <img src={load} alt="loading" />
       </div>
-    )
+    );
   }
 }
 
-export default Spinner
+export default Spinner;
